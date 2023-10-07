6a582573a992ea53b606209ec73cb400ad258225
liranderan <41143538+liranderan@users.noreply.github.com>
2023-10-07 18:49:49 +0800

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,99 +1,43 @@
-import React from 'react'
+import React, { memo } from 'react'
 import clsx from 'clsx'
 import Link from '@docusaurus/Link'
-import {
-  PageMetadata,
-  HtmlClassNameProvider,
-  ThemeClassNames,
-} from '@docusaurus/theme-common'
-import Layout from '@theme/Layout'
-import ResourceCard from './_components/ResourceCard'
-import BackToTopButton from '@theme/BackToTopButton'
-import { resourceData } from '@site/data/resource'
-import styles from './resource.module.css'
 
-function CategorySidebar() {
-  const sidebar = {
-    title: '',
-    items: resourceData.map(w => ({ title: w.name, permalink: `#${w.name}` })),
-  }
+import styles from './styles.module.css'
+import { type Resource } from '@site/data/resource'
+import Tooltip from '../../../project/_components/ShowcaseTooltip'
 
-  return (
-    <nav className={clsx(styles.sidebar, 'thin-scrollbar')}>
-      <div className={clsx(styles.sidebarItemTitle, 'margin-bottom--md')}>
-        {sidebar.title}
+const ResourceCard = memo(({ resource }: { resource: Resource }) => (
+  <li
+    key={resource.name}
+    className={clsx(styles.resourceCard, 'padding-vert--sm padding-horiz--md')}
+  >
+    <img
+      src={
+        typeof resource.logo === 'string'
+          ? resource.logo
+          : (resource.logo as any)?.src?.src
+      }
+      alt={resource.name}
+      className={clsx(styles.resourceCardImage)}
+    />
+    <div className={styles.resourceCardBody}>
+      <div className={clsx(styles.resourceCardHeader)}>
+        <h4 className={styles.resourceCardTitle}>
+          <Link href={resource.href} className={styles.resourceCardLink}>
+            {resource.name}
+          </Link>
+        </h4>
       </div>
-      <ul className={clsx(styles.sidebarItemList, 'clean-list')}>
-        {sidebar.items.map(item => (
-          <li key={item.permalink} className={styles.sidebarItem}>
-            <Link
-              isNavLink
-              to={item.permalink}
-              className={styles.sidebarItemLink}
-              activeClassName={styles.sidebarItemLinkActive}
-            >
-              {item.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  )
-}
-
-function CategoryList() {
-  return (
-    <div className={styles.category}>
-      {resourceData.map(cate => (
-        <div key={cate.name}>
-          <div className={styles.cateHeader}>
-            <h2 id={cate.name} className="anchor">
-              {cate.name}
-              <a
-                className="hash-link"
-                href={`#${cate.name}`}
-                title={cate.name}
-              ></a>
-            </h2>
-          </div>
-          <section>
-            <ul className={styles.resourceList}>
-              {cate.resources.map(resource => (
-                <ResourceCard key={resource.name} resource={resource} />
-              ))}
-            </ul>
-          </section>
-        </div>
-      ))}
+      <Tooltip
+        key={resource.name}
+        text={resource.desc}
+        anchorEl="#__docusaurus"
+        id={`tooltip-${resource.name}`}
+      >
+        <p className={styles.resourceCardDesc}>{resource.desc}</p>
+      </Tooltip>
     </div>
-  )
-}
+  </li>
+))
 
-export default function Resources() {
-  const title = '网址导航'
-  const description = '整合日常开发常用，推荐的网站导航页'
-
-  return (
-    <HtmlClassNameProvider
-      className={clsx(
-        ThemeClassNames.wrapper.blogPages,
-        ThemeClassNames.page.blogTagsListPage,
-      )}
-    >
-      <PageMetadata title={title} description={description} />
-      <Layout>
-        <div className="container margin-top--md">
-          <div className="row">
-            <aside className={clsx('col col--2')}>
-              <CategorySidebar />
-            </aside>
-            <main className="col col--10">
-              <CategoryList />
-            </main>
-          </div>
-        </div>
-        <BackToTopButton />
-      </Layout>
-    </HtmlClassNameProvider>
-  )
-}
+export default ResourceCard
